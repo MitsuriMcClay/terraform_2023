d8e9d1e388801b8c9b4fb9b7f7cee56aacbcc1b4
Mitsuri McClay <mcclay.mitsuri@gmail.com>
2023-07-18 11:37:29 +0900

    First commit

---
+++ aws_ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "terraform_sample_mitsuri" {
+  count                       = 2
+  ami                         = "ami-0d52744d6551d851e"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.terraform_sample_mitsuri[count.index].id
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.terraform_sample_mitsuri.id]
+  key_name                    = "terraform_key"
+  tags = {
+    Name = "${format("mitsuri%02d", count.index + 1)}"
+  }
+
+}
---
+++ var.tf
@@ -0,0 +1,16 @@
+#Vpc Cidr Block
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+#Availability Zone
+variable "availability_zone" {
+
+  type    = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c"]
+}
+
+#Subnet Cidr Block
+variable "subnet_cidr_block" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
